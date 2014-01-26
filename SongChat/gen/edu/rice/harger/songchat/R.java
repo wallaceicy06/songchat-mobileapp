c9f0d624b302a3b16df68147d6051f800c96d6df
Sean Harger <seanharger@me.com>
2014-01-26 04:47:26 -0600

    More stuff

--- SongChat/gen/edu/rice/harger/songchat/R.java
+++ SongChat/gen/edu/rice/harger/songchat/R.java
@@ -21,30 +21,36 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_send_now=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_play=0x7f020002;
+        public static final int ic_action_add_person=0x7f020000;
+        public static final int ic_action_new_email=0x7f020001;
+        public static final int ic_action_refresh=0x7f020002;
+        public static final int ic_action_send_now=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_play=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int email=0x7f080008;
+        public static final int action_settings=0x7f08000a;
+        public static final int add_friend=0x7f08000c;
+        public static final int email=0x7f080009;
+        public static final int friend_id=0x7f080007;
         public static final int listView1=0x7f080000;
         public static final int message=0x7f080001;
-        public static final int new_message=0x7f08000a;
-        public static final int refresh=0x7f08000b;
+        public static final int new_message=0x7f08000b;
+        public static final int refresh=0x7f08000d;
         public static final int review_button=0x7f080006;
-        public static final int send=0x7f08000c;
+        public static final int send=0x7f08000e;
         public static final int slice_player=0x7f080004;
         public static final int song_play_button=0x7f080002;
         public static final int toggle_button=0x7f080005;
-        public static final int username=0x7f080007;
+        public static final int username=0x7f080008;
         public static final int youtubeplayer=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_song_play=0x7f030001;
         public static final int activity_song_slice=0x7f030002;
-        public static final int dialog_signin=0x7f030003;
+        public static final int dialog_add_friend=0x7f030003;
+        public static final int dialog_signin=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- SongChat/src/edu/rice/harger/songchat/MainActivity.java
+++ SongChat/src/edu/rice/harger/songchat/MainActivity.java
@@ -4,13 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import edu.rice.harger.songchat.SongSliceActivity.FriendsDialog;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -36,7 +41,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	private String userId;
 	private ListView listView;
-
+	private String[] messageList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -64,6 +70,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.refresh:
 			refreshMessageList();
 			return true;
+		case R.id.add_friend:
+			AddFriendDialog dialog = AddFriendDialog.newInstance();
+			dialog.show(getFragmentManager().beginTransaction(), "dialog");
+			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
@@ -77,10 +87,48 @@ public class MainActivity extends Activity implements OnClickListener {
 		Intent newMessageIntent = new Intent(this, SongSliceActivity.class);
 		newMessageIntent.putExtra("vidId", "Zuich5TChEM");
 		newMessageIntent.putExtra("userId", userId);
-		Log.i("USERID", userId);
 		startActivity(newMessageIntent);
 	}
 
+	public void addFriend(final String friendId) {
+		Thread trd = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(
+						"http://ec2-184-73-80-30.compute-1.amazonaws.com/add_friend.php?username="
+								+ userId + "&friends=" + friendId);
+
+				// Making HTTP Request
+				try {
+					HttpResponse response = httpClient.execute(httpGet);
+
+					// writing response to log
+					InputStream in = response.getEntity().getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(in));
+					StringBuilder str = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						str.append(line);
+					}
+					in.close();
+
+					Log.d("New friend Response:", str.toString());
+
+				} catch (ClientProtocolException e) {
+					// writing exception to log
+					e.printStackTrace();
+
+				} catch (IOException e) {
+					// writing exception to log
+					e.printStackTrace();
+				}
+			}
+		});
+		trd.start();
+	}
+	
 	protected void loginWithCredentials(final String username,
 			final String email) {
 		Thread trd = new Thread(new Runnable() {
@@ -122,13 +170,18 @@ public class MainActivity extends Activity implements OnClickListener {
 		trd.start();
 	}
 
+	public void populateMessageList() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messageList);
+		listView.setAdapter(adapter);
+	}
+	
 	public void refreshMessageList() {
 		Thread trd = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				HttpClient httpClient = new DefaultHttpClient();
 				HttpGet httpGet = new HttpGet(
-						"http://ec2-184-73-80-30.compute-1.amazonaws.com/server2app.php?username=" + userId);
+						"http://ec2-184-73-80-30.compute-1.amazonaws.com/get_by_date.php?username=" + userId + "&number=10");
 
 				// Making HTTP Request
 				try {
@@ -145,9 +198,28 @@ public class MainActivity extends Activity implements OnClickListener {
 					}
 					in.close();
 					
-//					JSONObject json = new JSONObject(str);
-//					JSONArray articles = json.getJSONArray(name)
+					try {
+						JSONObject json = new JSONObject(str.toString());
 
+						String[] messages = new String[json.length()];
+						
+						Iterator<String> jsonIterator = json.keys();
+						int index = 0;
+						while(jsonIterator.hasNext()) {
+							messages[index++] = json.getJSONObject(jsonIterator.next()).get("sender").toString();
+						}
+						
+						messageList = messages;
+						populateMessageList();
+						
+						
+						
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					
+					
 					Log.d("Http Response:", str.toString());
 
 				} catch (ClientProtocolException e) {
@@ -192,6 +264,43 @@ public class MainActivity extends Activity implements OnClickListener {
 									.loginWithCredentials(username.getText()
 											.toString(), email.getText()
 											.toString());
+						}
+					});
+			builder.setNegativeButton("Cancel",
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+
+						}
+					});
+			return builder.create();
+		}
+	}
+	
+	public static class AddFriendDialog extends DialogFragment {
+		public static AddFriendDialog newInstance() {
+			AddFriendDialog dialog = new AddFriendDialog();
+			return dialog;
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			// Get the layout inflater
+			LayoutInflater inflater = getActivity().getLayoutInflater();
+			View inflatedView = inflater.inflate(R.layout.dialog_add_friend, null);
+			final EditText friendId = (EditText) inflatedView
+					.findViewById(R.id.friend_id);
+			builder.setView(inflatedView);
+
+			builder.setPositiveButton("Ok",
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							((MainActivity) getActivity())
+									.addFriend(friendId.getText().toString());
 						}
 					});
 			builder.setNegativeButton("Cancel",
